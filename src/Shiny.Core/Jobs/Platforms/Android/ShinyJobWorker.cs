87ba5bf759afb52fedf5fc9a10788fd508871b82
Diego Stamigni <diego.stamigni@gmail.com>
2019-07-31 12:45:05 +0100

    ShinyJobWorker implemented

--- src/Shiny.Core/Jobs/Platforms/Android/ShinyJobWorker.cs
+++ src/Shiny.Core/Jobs/Platforms/Android/ShinyJobWorker.cs
@@ -1,5 +1,9 @@
 ﻿#if ANDROID9
+using System.Linq;
+using System.Threading;
+using Android.Runtime;
 using System;
+using System.Threading.Tasks;
 using Android.Content;
 using AndroidX.Work;
 
@@ -8,17 +12,34 @@ namespace Shiny.Jobs
 {
     public class ShinyJobWorker : Worker
     {
+        CancellationTokenSource cancelSrc;
+
         public ShinyJobWorker(Context context, WorkerParameters workerParams) : base(context, workerParams)
         {
         }
 
-        //public ShinyJobWorker(IntPtr javaReference, JniHandleOwnership transfer) : base(javaReference, transfer)
-        //{
-        //}
+        protected ShinyJobWorker(IntPtr javaReference, JniHandleOwnership transfer) : base(javaReference, transfer)
+        {
+        }
 
         public override Result DoWork()
         {
+            this.cancelSrc = new CancellationTokenSource();
+
+            var results = Task.Run(() => ShinyHost
+                .Resolve<IJobManager>()
+                .RunAll(this.cancelSrc.Token)).GetAwaiter().GetResult().ToList();
+            if (results.Any(x => !x.Success))
+            {
+                return Result.InvokeFailure();
+            }
+
             return Result.InvokeSuccess();
+        }
+
+        public override void OnStopped()
+        {
+            this.cancelSrc?.Cancel();
         }
     }
 }
